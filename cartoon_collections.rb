7564d3918710348f96051ac217aa7747dbd95ddd
Adrienne Yao <adrienne.yao@icloud.com>
2017-11-03 19:54:18 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,7 @@ def summon_captain_planet(elements)# code an argument here
 
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(assorted_words)# code an argument here
   # Your code here
 end
 
