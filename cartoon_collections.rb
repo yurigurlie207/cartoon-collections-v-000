99db413cec4d13478ca4553c61ee682db7f6c86f
Adrienne Yao <adrienne.yao@icloud.com>
2017-11-03 21:22:30 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,11 +21,9 @@ end
 def find_the_cheese(foodList)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  i = 0
-  cheese_types.detect { |cheese|
-    foodList[i] == cheese
-    i =+1
-  }
+  
+    cheese_types.detect { |cheese|
+      foodList[i] == cheese
+    }
 
 end
- 
