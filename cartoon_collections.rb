fad0e86140ae8759e3ff7f79f2947720284cadda
Adrienne Yao <adrienne.yao@icloud.com>
2017-11-03 19:40:02 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
-  dwarves.each_with_index { |item, index| puts "#{index}. #{item}"}
+  dwarves.each_with_index { |name, index| puts "#{index}. #{name}"}
     end
   end
 end
